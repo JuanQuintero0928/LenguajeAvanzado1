3eb9d5ca46725d7c1df7b5907c93ae5c57227639
Juan Jose <jjquintero0928@gmail.com>
2022-08-09 18:01:54 -0500

    se agrega codigo ejercicio6.py

--- Clase 1/ejercicio6.py
+++ Clase 1/ejercicio6.py
@@ -1,4 +1,12 @@
+import math
+
+
 a = int(input("Digite el valor de a: "))
 b = int(input("Digite el valor de b: "))
 c = int(input("Digite el valor de c: "))
 
+oper1 = (b**2) - (4 * (a * c))
+raiz1 = math.sqrt(oper1)
+solucion1 = ((b)+ raiz1) / (2 * a)
+
+print(solucion1)
