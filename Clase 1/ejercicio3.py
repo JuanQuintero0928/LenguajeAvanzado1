ffbb9cd5cde9790c793c2a0d438f9cd20842a42e
Juan Jose <jjquintero0928@gmail.com>
2022-08-10 17:25:31 -0500

    se agrega funcionalidad al archivo ejercicio3.py

--- Clase 1/ejercicio3.py
+++ Clase 1/ejercicio3.py
@@ -1,15 +1,20 @@
-terminos = int(input("Digite un numero para hallar el valor de pi : "))
+try:
+    terminos = int(input("Digite un numero para hallar el valor de pi : "))
+    if terminos > 0:
+        denominador = 3
+        resultado = 1
+            
+        for i in range (terminos):
+            division = 1 / denominador
+            denominador = denominador + 2
+            if i % 2 == 0:
+                resultado = resultado - division
+            else:
+                resultado = resultado + division
 
-denominador = 3
-resultado = 1
-    
-for i in range (terminos):
-    division = 1 / denominador
-    denominador = denominador + 2
-    if i % 2 == 0:
-        resultado = resultado - division
+        pi = resultado * 4
+        print("El resultado de pi es de : " + str(pi))
     else:
-        resultado = resultado + division
-
-pi = resultado * 4
-print("El resultado de pi es de : " + str(pi))
+        print("El numero debe ser mayor a cero.")
+except ValueError:
+    print("Solo se recibe como dato de entrada valores numericos.")
