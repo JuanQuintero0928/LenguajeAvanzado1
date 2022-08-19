51bf5652f55ab07016553696cfc6aa1b0673ba39
Juan Linux <jjquintero0928@gmail.com>
2022-08-18 23:13:03 -0500

    se agrega solucion al parcial

--- Parcial1/geometria.py
+++ Parcial1/geometria.py
@@ -1,53 +1,129 @@
 import math
 
+
 class Geometria():
 
-    #Metodo Constructor:
+    # Metodo Constructor:
     def __init__(self):
         self.dato = None
         self.area = None
         self.altura = None
         self.radio = None
+        self.generatriz = None
 
     def mostrar_menu(self):
-        self.dato = int(input("""------------ MENU ------------
-            1. Hallar el Area y Volumen de un Cubo.
-            2. Hallar el Area y Volumen de un Cilindro.
-            3. Hallar el Area y Volumen de una Esfera.
-            4. Hallar el Area y Volumen de un Cono.
-            5. Salir
-            
-            Digite su opcion -> """))
+        try:
+            self.dato = int(input("""
 
-        if self.dato == 1:
-            u.cubo()
-        if self.dato == 2:
-            u.cilindro()
-        if self.dato == 3:
-            u.esfera()
-            
+            ------------ MENU ------------
+                1. Hallar el Area y Volumen de un Cubo.
+                2. Hallar el Area y Volumen de un Cilindro.
+                3. Hallar el Area y Volumen de una Esfera.
+                4. Hallar el Area y Volumen de un Cono.
+                5. Salir
+
+                Digite su opcion -> """))
+
+            if self.dato == 1:
+                u.cubo()
+            if self.dato == 2:
+                u.cilindro()
+            if self.dato == 3:
+                u.esfera()
+            if self.dato == 4:
+                u.cono()
+            if self.dato == 5:
+                print("Saliendo ... ")
+                exit()
+            if self.dato > 5 or self.dato < 1:
+                print(
+                    "la opcion ingresada no se encuentra en el menu, Digite de nuevo una opcion.")
+                u.mostrar_menu()
+        except ValueError:
+            print("Solo se aceptan valores numericos. Digite de nuevo una opcion.")
+            u.mostrar_menu()
+
     def cubo(self):
-        self.area = int(input("Digite el area de un lado del cubo : "))
-        AreaCubo = 6 * (self.area**2)
-        VolumenCubo = self.area**3
-        print("El Area total del Cubo es de : " + str(AreaCubo))
-        print("El Volumen total del Cubo es de : " + str(VolumenCubo))
+        print("""
+            ------------ CUBO ------------
+            """)
+        try:
+            self.area = float(input("Digite el area de un lado del cubo : "))
+            if self.area > 0:
+                AreaCubo = 6 * (self.area**2)
+                VolumenCubo = self.area**3
+                print("El Area total del Cubo es de : " + str(round(AreaCubo, 2)))
+                print("El Volumen total del Cubo es de : " + str(round(VolumenCubo, 2)))
+                u.mostrar_menu()
+            else:
+                print("El numero debe ser mayor a 0.")
+                u.cubo()
+        except ValueError:
+            print("Solo se aceptan valores numericos.")
+            u.cubo()
 
     def cilindro(self):
-        self.altura = int(input("Digite la altura del cilindro : "))
-        self.radio = int(input("Digite el radio del cilindro : "))
-        AreaCilindro = 2 * math.pi * self.radio * (self.altura + self.radio)
-        VolumenCilindro = math.pi * (self.radio**2) * self.altura 
-        print("El Area total del Cilindro es de : " + str(AreaCilindro))
-        print("El Volumen total del Cilindro es de : " + str(VolumenCilindro))
+        print("""
+            ------------ CILINDRO ------------
+            """)
+        try:
+            self.altura = float(input("Digite la altura del cilindro : "))
+            self.radio = float(input("Digite el radio del cilindro : "))
+            if self.altura > 0 and self.radio > 0:
+                AreaCilindro = 2 * math.pi * \
+                    self.radio * (self.altura + self.radio)
+                VolumenCilindro = math.pi * (self.radio**2) * self.altura
+                print("El Area total del Cilindro es de : " + str(round(AreaCilindro, 2)))
+                print("El Volumen total del Cilindro es de : " + str(round(VolumenCilindro, 2)))
+                u.mostrar_menu()
+            else:
+                print("Los numeros deben ser mayores a 0.")
+                u.cilindro()
+        except ValueError:
+            print("Solo se aceptan valores numericos")
+            u.cilindro()
 
     def esfera(self):
-        pass
+        print("""
+            ------------ ESFERA ------------
+            """)
+        try:
+            self.radio = float(input("Digite el radio de la esfera : "))
+            if self.radio > 0:
+                AreaEsfera = 4 * math.pi * self.radio**2
+                VolumenEsfera = (4/3) * math.pi * self.radio**3
+                print("El Area de la Esfera es de : " + str(round(AreaEsfera,2)))
+                print("El Volumen de la Esfera es de : " + str(round(VolumenEsfera,2)))
+                u.mostrar_menu()
+            else:
+                print("El numero debe ser mayor a 0.")
+                u.esfera()
+        except ValueError:
+            print("Solo se aceptan valores numericos")
+            u.esfera()
 
     def cono(self):
-        pass
+        print("""
+            ------------ CONO ------------
+            """)
+        try:
+            self.radio = float(input("Digite el radio del Cono : "))
+            self.generatriz = float(input("Digite la generatriz del Cono : "))
+            self.altura = float(input("Ingrese la altura del Cono : "))
+            if self.radio > 0 and self.generatriz > 0 and self.altura > 0:
+                AreaCono = math.pi * self.radio**2 + math.pi * self.radio * self.generatriz
+                VolumenCono = (math.pi * self.radio**2 * self.altura)/3
+                print("El Area del Cono es de : " + str(round(AreaCono,2)))
+                print("El Volumen del Cono es de : " + str(round(VolumenCono,2)))
+                u.mostrar_menu()
+            else:
+                print("El numero debe ser mayor a 0.")
+                u.cono()
+        except ValueError:
+            print("Solo se aceptan valores numericos.")
+            u.cono()
+
 
 if __name__ == "__main__":
     u = Geometria()
     u.mostrar_menu()
-    
